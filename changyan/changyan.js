f985d4a6972f3f22ad3a36cca7e2b859ecdfe1e3
codepiano <codepiano.li@gmail.com>
2014-09-01 15:11:19 +0800

    bug修复

--- changyan/changyan.js
+++ changyan/changyan.js
@@ -6,18 +6,23 @@ require(["gitbook", "jQuery"], function(gitbook, $) {
             sourceId = sourceId + 'index.html';
         }
         $(".book-body .page-inner").append('<div id="SOHUCS" sid="' + sourceId + '"></div>');
-        var appid = window.changyan.appid, conf = window.changyan.conf;
-        var doc = document,
-        s = doc.createElement('script'),h = doc.getElementsByTagName('head')[0] || doc.head || doc.documentElement;
-        s.type = 'text/javascript';
-        s.charset = 'utf-8';
-        s.src =  'http://assets.changyan.sohu.com/upload/changyan.js?conf='+ conf +'&appid=' + appid;
-        h.insertBefore(s,h.firstChild);
+        if (window.changyan) {
+            var appid = window.changyan.appid;
+            var conf = window.changyan.conf;
+            var doc = document,
+            s = doc.createElement('script'),h = doc.getElementsByTagName('head')[0] || doc.head || doc.documentElement;
+            s.type = 'text/javascript';
+            s.charset = 'utf-8';
+            s.src =  'http://assets.changyan.sohu.com/upload/changyan.js?conf='+ conf +'&appid=' + appid;
+            h.insertBefore(s,h.firstChild);
+        } else {
+            console.log('无法获取畅言config');
+        }
     }
 
     gitbook.events.bind("start", function(e, config){
         document.location.protocol == 'https:' ? 'https:' : 'http:';
-        window.changyan = config;
+        window.changyan = config.changyan;
     });
 
     gitbook.events.bind("page.change", function(e){
